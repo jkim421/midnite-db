6c379fd65dd760afede80b5e66122c93cc7d8766
john.kim <jkim21@pm.me>
2023-09-10 13:35:21 -0400

    Extract MainHeaderAbout

---
+++ frontend/src/components/Main/MainHeaderAbout.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+import { CloseIcon } from '../icons';
+
+const MainHeaderAbout = () => {
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+
+  let aboutClassNames = 'app-header_about-modal';
+
+  if (isAboutOpen) {
+    aboutClassNames = aboutClassNames.concat(' app-header_about-modal-open');
+  }
+
+  const onClick = () => setIsAboutOpen(!isAboutOpen);
+
+  return (
+    <>
+      <div
+        className="app-header_about-btn"
+        onClick={onClick}>
+        <span>?</span>
+      </div>
+      <div className={aboutClassNames}>
+        <div style={{ padding: '12px 12px 24px 24px' }}>
+          <div className="about-header_about-header">
+            <div>About midnite-db</div>
+            <CloseIcon
+              onClick={onClick}
+              style={{
+                height: 16,
+                width: 24,
+                margin: 0,
+                padding: 12,
+                justifyContent: 'center',
+              }}
+            />
+          </div>
+          <div>
+            <span>By&nbsp;</span>
+            <a
+              className="about-header_profile-link"
+              href="https://github.com/jkim421"
+              target="blank">
+              <span>John Kim</span>
+            </a>
+          </div>
+          <div style={{ marginTop: 24, textDecoration: 'underline' }}>
+            Filters Guide
+          </div>
+          <div>
+            <div className="about-header_about-section-title">OR Filters</div>
+            <div className="about-header_about-section-subtitle">
+              Option A OR Option B OR ...
+            </div>
+            <ul className="app-header_about-list">
+              <li>Media Type</li>
+              <li>Status</li>
+              <li>Rating</li>
+              <li>Demographic</li>
+            </ul>
+          </div>
+          <div>
+            <div className="about-header_about-section-title">
+              AND/OR Filters
+            </div>
+            <div className="about-header_about-section-subtitle">
+              <div>If no sets: Option A AND Option B AND ...</div>
+              <div>If 1+ sets: Set A OR Set B OR ...</div>
+            </div>
+            <ul className="app-header_about-list">
+              <li>Genre</li>
+              <li>Theme</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainHeaderAbout;
